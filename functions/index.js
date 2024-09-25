14df4455a6ab52f357889383c4e7af2ea9a37b9d
lucas <lucasalves.souza01@gmail.com>
2024-09-25 18:12:47 -0300

    chore: adding firebase hosting setup

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,6 @@ const isDev = process.env.NODE_ENV !== 'production';
 const app = next({ dev: isDev });
 const handle = app.getRequestHandler();
 
-exports.nextApp = functions.https.onRequest((req, res) => {
+exports.nextApp = functions.region('us-west1').https.onRequest((req, res) => {
   return app.prepare().then(() => handle(req, res));
 });
